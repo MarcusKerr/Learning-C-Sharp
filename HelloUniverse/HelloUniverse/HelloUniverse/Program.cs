deb7193c29fb1d990489d99b43988523f411b5dd
Marcus <marcus@Iris-Ver32M.local>
2018-03-10 12:26:30 +0000

    First commit

---
+++ HelloUniverse/HelloUniverse/HelloUniverse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloUniverse
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Universe!");
+        }
+    }
+}
